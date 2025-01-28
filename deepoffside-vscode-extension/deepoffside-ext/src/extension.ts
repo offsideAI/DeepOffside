ba0de28cc593879c6cd41c5edd74efe5542fe78c
Arunabh Das <arunabhdas@gmail.com>
2025-01-28 18:01:18 -0500

    Added greeting command

--- deepoffside-vscode-extension/deepoffside-ext/src/extension.ts
+++ deepoffside-vscode-extension/deepoffside-ext/src/extension.ts
@@ -11,14 +11,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('deepoffside-ext" is now active!');
 
-	const disposable = vscode.commands.registerCommand('deepoffside-ext.greeting', () => {
-		vscode.window.showErrorMessage('Hello Ladies and Gentlemen')
-	});
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('deepoffside-ext.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('deepoffside-ext.greeting', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from deepoffside-ext!');
