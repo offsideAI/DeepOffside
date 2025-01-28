1d54ad25cc9ed68761039567cc6689e334512797
Arunabh Das <arunabhdas@gmail.com>
2025-01-28 17:53:19 -0500

    Registered greeting

--- deepoffside-vscode-extension/deepoffside-ext/src/extension.ts
+++ deepoffside-vscode-extension/deepoffside-ext/src/extension.ts
@@ -2,13 +2,18 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "deepoffside-ext" is now active!');
+	console.log('deepoffside-ext" is now active!');
+
+	const disposable = vscode.commands.registerCommand('deepoffside-ext.greeting', () => {
+		vscode.window.showErrorMessage('Hello Ladies and Gentlemen')
+	});
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
